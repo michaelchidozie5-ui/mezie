0293e71271ff2773660058a9f3f4304cb214a3df
unknown <michaelchidozie5@gmail.com>
2026-07-04 17:07:10 +0100

    Point Capacitor to live vercel URL

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,11 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.mezie.studio',
   appName: 'Mezie Studio',
-  webDir: 'out'
+  webDir: 'out',
+  server: {
+    url: 'https://mezie-studio.vercel.app',
+    cleartext: true
+  }
 };
 
 export default config;
